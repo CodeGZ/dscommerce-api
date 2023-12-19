01e5c741f61239d59f5ca340223fcade20f72dcc
Geziel Bueno <gezielbueno1@gmail.com>
2023-12-19 08:46:31 -0300

    PUT Product

--- src/main/java/com/devsup/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsup/dscommerce/controllers/ProductController.java
@@ -18,6 +18,7 @@ public class ProductController {
 
     @Autowired
     private ProductService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
         ProductDTO dto = service.findById(id);
@@ -36,5 +37,11 @@ public class ProductController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
